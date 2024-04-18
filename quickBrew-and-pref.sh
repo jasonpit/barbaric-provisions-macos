cca9c24e371de4b3086e3425df76b52c840ea6db
Jason <jasonpit@me.com>
2024-04-18 12:33:47 -0700

    Update quickBrew-and-pref.sh

--- quickBrew-and-pref.sh
+++ quickBrew-and-pref.sh
@@ -53,7 +53,7 @@ brew install gh
 brew install --cask microsoft-teams
 brew install --cask slack
 
-#### Add some fun 
+#### ohmyzsh installation 
 
 # Check and install Oh My Zsh
 if [ ! -d "$HOME/.oh-my-zsh" ]; then
@@ -63,13 +63,39 @@ else
     echo "Oh My Zsh is already installed."
 fi
 
-# Add blank spaces to the Dock
+# Setup custom aliases
+ALIAS_FILE="$HOME/.oh-my-zsh/custom/aliases.zsh"
+if [ ! -f "$ALIAS_FILE" ]; then
+    touch "$ALIAS_FILE"
+fi
+
+cat >> "$ALIAS_FILE" <<EOF
+#### zsh aliases 
+## list all properly like in Linux.
+alias ll='ls -al'
+## open my alias file in vim (quick way to add or view current aliases) 
+alias aliases='vim ~/.oh-my-zsh/custom/aliases.zsh'
+## run apple softwareupdates and then run brew updates.
+alias update='sudo softwareupdate -ia --verbose && brew upgrade'
+## give me all of my IP addresses, internal LAN and WIFI as well as external.
+alias ip='curl ifconfig.co && ipconfig getifaddr en0 && ipconfig getifaddr en1'
+## give me a random comoplex password
+alias pw='openssl rand -base64 32'
+EOF
+
+echo "Aliases have been added to $ALIAS_FILE"
+# Ensure the alias file is sourced by .zshrc
+if ! grep -q "source $ALIAS_FILE" "$HOME/.zshrc"; then
+    echo "source $ALIAS_FILE" >> "$HOME/.zshrc"
+fi
+
+# Add some blank spaces to the macOS Dock
 for i in {1..5}; do
   defaults write com.apple.dock persistent-apps -array-add '{"tile-type"="spacer-tile";}'
 done
 killall Dock
 
-# Change default location for screenshots
+# Change the default location for screenshots
 mkdir -p "$HOME/Pictures/ScreenShots"
 defaults write com.apple.screencapture location "$HOME/Pictures/ScreenShots"
 killall SystemUIServer
